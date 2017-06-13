01e117563918786172e42bee8019e9a68f2d8c6b
Gebs <gebs.dev@gmail.com>
2017-06-13 23:16:52 +0200

    Etape 1 : Creation Bdd, Affichage calendrier avec bloquage des jours passés, des dimanches, des jours fériés

--- src/AppBundle/Controller/TicketController.php
+++ src/AppBundle/Controller/TicketController.php
@@ -5,10 +5,15 @@ namespace AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 class TicketController extends Controller {
 
+    /**
+     * @Route("/ticketing", name="ticketing")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('AppBundle:Ticket:index.html.twig');
--- web/js/displayDatePicker.js
+++ web/js/displayDatePicker.js
@@ -1,14 +1,37 @@
 
+/*
+ * Affichage du calendrier DatePicker
+ *  avec en date de départ, la date d'aujourd'hui
+ *  Un réservation maximale sur 2 mois
+ *  Les dimanches (0) et les mardis (2) désactivés
+ */
+
 $( function() {
+
+    // get the current date
 
     $.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
     $("#datePicker").datepicker({
         minDate: 0,
-        maxDate: "+2m"
+
+        // Désactivation des mardis et dimanche
+        beforeShowDay: function(date) {
+            if (date.getDay() === 0 || date.getDay() === 2 || ((date.getMonth() === 4) && (date.getDate() === 1))
+                || ((date.getMonth() === 10) && (date.getDate() === 1)) ||
+                ((date.getDate() === 25) && (date.getMonth() === 11)))
+            {
+                return [false, ''];
+            } else
+            {
+                return [true, ''];
+            }
+        }
         }
 
+
+
+
+
     );
-
-
 
 } );
